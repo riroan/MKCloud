443e22be12b24c36d8fae3481bf4ecb548989878
riroan <riroan@naver.com>
2023-01-29 06:33:19 +0900

    test: 파일 업로드 테스트

---
+++ backend/src/test/java/com/openbox/backend/repository/FileRepositoryTest.java
@@ -0,0 +1,85 @@
+package com.openbox.backend.repository;
+
+import com.openbox.backend.domain.FileEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@Transactional
+@SpringBootTest
+class FileRepositoryTest {
+    @Autowired
+    FileRepository fileRepository;
+
+    @AfterEach
+    void afterEach() {
+        if (fileRepository instanceof MemoryFileRepository){
+            ((MemoryFileRepository)fileRepository).clearStore();
+        }
+    }
+
+    @Test
+    @DisplayName(value = "파일 하나 저장")
+    void save() {
+        // given
+        FileEntity file = new FileEntity("file1.jpg", "store_file1.jpg", 10L, "user1");
+
+        // when
+        FileEntity savedFile = fileRepository.save(file);
+
+        //then
+        FileEntity findFile = fileRepository.findById(savedFile.getId());
+        assertThat(findFile).isEqualTo(file);
+    }
+
+    @Test
+    @DisplayName(value = "파일 전부 찾기")
+    void findAll() {
+        FileEntity file1 = new FileEntity("file1.jpg", "store_file1.jpg", 10L, "user1");
+        FileEntity file2 = new FileEntity("file2.jpg", "store_file2.jpg", 10L, "user1");
+        FileEntity file3 = new FileEntity("file3.jpg", "store_file3.jpg", 10L, "user1");
+
+        FileEntity savedFile1 = fileRepository.save(file1);
+        FileEntity savedFile2 = fileRepository.save(file2);
+        FileEntity savedFile3 = fileRepository.save(file3);
+
+        List<FileEntity> result = fileRepository.findAll();
+        assertThat(result).containsExactly(savedFile1, savedFile2, savedFile3);
+    }
+
+    @Test
+    @DisplayName(value = "작성자로 찾기")
+    void findByOwner() {
+        FileEntity file1 = new FileEntity("file1.jpg", "store_file1.jpg", 10L, "user1");
+        FileEntity file2 = new FileEntity("file2.jpg", "store_file2.jpg", 10L, "user2");
+        FileEntity file3 = new FileEntity("file3.jpg", "store_file3.jpg", 10L, "user3");
+        FileEntity file4 = new FileEntity("file4.jpg", "store_file4.jpg", 10L, "user1");
+        FileEntity file5 = new FileEntity("file5.jpg", "store_file5.jpg", 10L, "user2");
+
+        FileEntity savedFile1 = fileRepository.save(file1);
+        FileEntity savedFile2 = fileRepository.save(file2);
+        FileEntity savedFile3 = fileRepository.save(file3);
+        FileEntity savedFile4 = fileRepository.save(file4);
+        FileEntity savedFile5 = fileRepository.save(file5);
+
+        List<FileEntity> result1 = fileRepository.findByOwner("user1");
+        assertThat(result1).containsExactly(savedFile1, savedFile4);
+
+        List<FileEntity> result2 = fileRepository.findByOwner("user2");
+        assertThat(result2).containsExactly(savedFile2, savedFile5);
+
+        List<FileEntity> result3 = fileRepository.findByOwner("user3");
+        assertThat(result3).containsExactly(savedFile3);
+    }
+}
