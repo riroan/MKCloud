0b9d709bc2b63e9cd04c15af9c6b5289c5c52f72
riroan <riroan@naver.com>
2023-01-28 13:42:41 +0900

    feat: 시간측정 AOP

--- backend/src/main/java/com/openbox/backend/aop/TimeTraceAop.java
+++ backend/src/main/java/com/openbox/backend/aop/TimeTraceAop.java
@@ -1,23 +1,28 @@
 package com.openbox.backend.aop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@Slf4j
 @Component
 @Aspect
 public class TimeTraceAop {
     @Around("execution(* com.openbox.backend..*(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
-        System.out.println("START: " + joinPoint.toString());
+        String uuid = UUID.randomUUID().toString();
+        log.info("[{}]START: {}", uuid, joinPoint.toString());
         try {
             return joinPoint.proceed();
         } finally {
             long finish = System.currentTimeMillis();
             long timeMs = finish - start;
-            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+            log.info("[{}]END: {} {}ms", uuid, joinPoint, timeMs);
         }
     }
 }
