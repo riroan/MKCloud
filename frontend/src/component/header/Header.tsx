997b84b4325f8a9d0ab0ca183da16361c1912ff1
riroan <riroan@naver.com>
2023-02-14 23:47:39 +0900

    feat: 반응형 웹 3차 업데이트

--- frontend/src/component/header/Header.tsx
+++ frontend/src/component/header/Header.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './Header.module.scss'
 import classnames from 'classnames/bind'
+import MenuIcon from '@mui/icons-material/Menu'
 const cx = classnames.bind(styles)
 
 type HeaderProps = {
-    title?: string,
-    className?: string
+	title?: string
+	className?: string
 }
 
 export default function Header({ title, className }: HeaderProps) {
-    return (
+	return (
 		<div className={cx('header', className)}>
-            <span className={ cx('title')}>{title}</span>
+			<span className={cx('title')}>{title}</span>
+			<MenuIcon className={cx('icon')} />
 		</div>
 	)
 }
